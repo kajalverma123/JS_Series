20f6fb9d7af391f134a0ccec4148d39b286b49d7
kajalverma123 <142680933+kajalverma123@users.noreply.github.com>
2025-05-11 07:27:56 +0000

    Function Basic

---
+++ 02_Basics/function.js
@@ -0,0 +1,19 @@
+function addTwoNumbers(num1,num2)
+{//console.log("Hey")
+    return num1+num2;
+ //   console.log("Hello")
+}
+//addTwoNumbers();
+//addTwoNumbers(4,7);
+//addTwoNumbers(4,"a");
+const result=addTwoNumbers(8,12)
+console.log("Result :", result)
+
+function loginUserMessage(username)
+{
+    return popup=`hey ${username} you logged in successfully`;
+}
+console.log(loginUserMessage())
+console.log(loginUserMessage("Vidyanshi"))
+//loginUserMessage("Kajal")
+//console.log(popup)
