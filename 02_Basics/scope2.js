74234e2977c2064e228a9094b9f0c38f695ced1c
kajalverma123 <142680933+kajalverma123@users.noreply.github.com>
2025-05-13 15:53:29 +0000

    Scope level and mini hoisting

---
+++ 02_Basics/scope2.js
@@ -0,0 +1,29 @@
+function one(){
+    const username="Kajal"
+    
+        function two()
+        {
+            const website="  Youtube"
+            console.log(username)
+        }
+        //console.log(website)
+        two()
+        //console.log(website)
+    }
+    one()
+
+    console.log(addone(4))
+    function addone(num)
+    {
+        return num +1;
+
+    }
+    //console.log(addone(3))
+
+console.log(addTwo(6))
+    const addTwo=function(num)
+    {
+        return num+2;
+    }
+    //console.log(addTwo(7))
+
