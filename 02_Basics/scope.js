155d8366b8810238d1b8c2b3670bfce76dd8200e
kajalverma123 <142680933+kajalverma123@users.noreply.github.com>
2025-05-13 07:33:54 +0000

    Global and Local Scope

---
+++ 02_Basics/scope.js
@@ -0,0 +1,14 @@
+const a=300
+let b=200
+var c=400
+if(true)
+{let b=234
+    var c=800
+    const a=100
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a);
+console.log(b);
+console.log(c);
